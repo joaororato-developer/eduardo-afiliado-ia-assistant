e37d68af8aa7098e9d242949ed9cd62f0ca9f273
João Vitor Machado Rorato <124640745+joaororato-developer@users.noreply.github.com>
2026-04-22 10:57:28 -0300

    Update app.ts

--- api/app.ts
+++ api/app.ts
@@ -133,7 +133,7 @@ app.post('/receive-whatsapp', (req: Request, res: Response) => {
 
 			logger.info("Chamando Gemini")
 			const model = genAI.getGenerativeModel({
-				model: "gemini-2.5-flash",
+				model: "gemini-1.5-flash",
 			});
 
 			const result = await model.generateContent({
